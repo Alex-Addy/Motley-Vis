828a5f7b23385e6c84834d711aa4b11c1347a189
Alexander Addy <alex.addy@gmail.com>
2014-11-09 13:31:13 -0500

    Added unit testing for DataRowProvider

---
+++ Motley Vis Unit Tests/DataRowProviderUnitTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Motley_Vis;
+
+namespace Motley_Vis_Unit_Tests
+{
+    [TestClass]
+    public class DataRowProviderUnitTests
+    {
+        [TestMethod]
+        public void CommaDelimitedTest()
+        {
+            var newProvider = new DataRowProvider(@"E:\Test_Data\small_csv\people_5_col_with_headers.csv", new char[] {','});
+
+            Assert.AreEqual(5, newProvider.Count);
+        }
+    }
+}
